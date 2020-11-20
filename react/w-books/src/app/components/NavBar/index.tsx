23cb6c2c7ff0281b4ba87c4ff7879f87eed2e075
Maximiliano Cóppola <maxicopp@gmail.com>
2020-11-20 02:16:45 -0300

    UPDATE
    Delete warnings in console

--- react/w-books/src/app/components/NavBar/index.tsx
+++ react/w-books/src/app/components/NavBar/index.tsx
@@ -17,7 +17,7 @@ function NavBar() {
     <div className={styles.navbarContainer}>
       <img src={logo} alt="Logo" className={styles.logo} />
 
-      <button className={styles.nabvarLogout} onClick={handleLogout}>
+      <button type="button" className={styles.nabvarLogout} onClick={handleLogout}>
         {i18next.t('Navbar:login')}
       </button>
     </div>
